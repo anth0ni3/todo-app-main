0c9df4a802e1079e8610d1ed6e070c009e171790
Anthony Onu <seboh44@gmail.com>
2021-08-22 09:34:55 +0100

    Fixed Build and Updated Empty State

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,11 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import TodoItem from "./TodoItem";
 import InputForm from "./InputForm";
-import {TodoAppContext} from "./context/TodoApp.context";
+import { TodoAppContext } from "./context/TodoApp.context";
 import "./assets/css/TodoList.css";
 
 const TodoList = () => {
-  const {todos, showAdd} = useContext(TodoAppContext);
-
+  const { todos, showAdd, toggleAdd } = useContext(TodoAppContext);
 
   return (
     <div>
@@ -17,27 +16,20 @@ const TodoList = () => {
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
-            viewBox="0 0 24 24"
-          >
+            viewBox="0 0 24 24">
             <title>Add new todo</title>
             <g fill="#111111">
-              <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"/>
+              <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z" />
             </g>
           </svg>
         </div>
       </div>
       <div>
-        {todos.length > 0 ? (
-          todos.map((todo) => (
-            <TodoItem
-              id={todo.id}
-              key={todo.id}
-              task={todo.task}
-            />
-          ))
-        ) : (
-          <p className="flexy item">Add new todo</p>
-        )}
+        {todos.length > 0
+          ? todos.map(todo => (
+              <TodoItem id={todo.id} key={todo.id} task={todo.task} />
+            ))
+          : !showAdd && <p className="flexy item">Add new todo</p>}
         {showAdd ? <InputForm /> : ""}
       </div>
     </div>
